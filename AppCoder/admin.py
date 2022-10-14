cb44c82a8f19c770b78ef27cb2bc51a29f04f4ae
E.A <emanuel.alvz@gmail.com>
2022-10-13 23:54:32 -0300

    Web funcionando V2.0

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from AppCoder.models import *
 
 # Register your models here.
+
+
+admin.site.register(About)
+admin.site.register(LatestWork)
+admin.site.register(Comunidad)
+admin.site.register(Contacto)
